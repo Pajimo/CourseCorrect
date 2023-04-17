ec6ed6100b27db93409fa3007fd6ec9a1b2e5596
Pajimo <faniyanolamide@gmail.com>
2023-04-17 23:16:26 +0100

    project setup

---
+++ loginForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace CourseCorrect
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.loginUsernameLabel = new System.Windows.Forms.Label();
+            this.loginPasswordLabel = new System.Windows.Forms.Label();
+            this.loginUsernameTextbox = new System.Windows.Forms.TextBox();
+            this.loginPasswordTextbox = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(142, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(501, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Log in to CourseCorrect";
+            // 
+            // loginUsernameLabel
+            // 
+            this.loginUsernameLabel.AutoSize = true;
+            this.loginUsernameLabel.Font = new System.Drawing.Font("Segoe UI Historic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.loginUsernameLabel.Location = new System.Drawing.Point(142, 163);
+            this.loginUsernameLabel.Name = "loginUsernameLabel";
+            this.loginUsernameLabel.Size = new System.Drawing.Size(114, 30);
+            this.loginUsernameLabel.TabIndex = 1;
+            this.loginUsernameLabel.Text = "Username";
+            // 
+            // loginPasswordLabel
+            // 
+            this.loginPasswordLabel.AutoSize = true;
+            this.loginPasswordLabel.Font = new System.Drawing.Font("Segoe UI Historic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.loginPasswordLabel.Location = new System.Drawing.Point(142, 228);
+            this.loginPasswordLabel.Name = "loginPasswordLabel";
+            this.loginPasswordLabel.Size = new System.Drawing.Size(108, 30);
+            this.loginPasswordLabel.TabIndex = 1;
+            this.loginPasswordLabel.Text = "Password";
+            // 
+            // loginUsernameTextbox
+            // 
+            this.loginUsernameTextbox.Font = new System.Drawing.Font("Segoe UI Historic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.loginUsernameTextbox.Location = new System.Drawing.Point(325, 163);
+            this.loginUsernameTextbox.Name = "loginUsernameTextbox";
+            this.loginUsernameTextbox.Size = new System.Drawing.Size(295, 35);
+            this.loginUsernameTextbox.TabIndex = 2;
+            // 
+            // loginPasswordTextbox
+            // 
+            this.loginPasswordTextbox.Font = new System.Drawing.Font("Segoe UI Historic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.loginPasswordTextbox.Location = new System.Drawing.Point(325, 223);
+            this.loginPasswordTextbox.Name = "loginPasswordTextbox";
+            this.loginPasswordTextbox.Size = new System.Drawing.Size(295, 35);
+            this.loginPasswordTextbox.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe UI Historic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox1.Location = new System.Drawing.Point(250, 337);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(83, 25);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Student";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Segoe UI Historic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox2.Location = new System.Drawing.Point(465, 337);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(75, 25);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Admin";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Historic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(316, 304);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Account Type";
+            // 
+            // btn_login
+            // 
+            this.btn_login.Font = new System.Drawing.Font("Segoe UI Historic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_login.Location = new System.Drawing.Point(325, 387);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(134, 44);
+            this.btn_login.TabIndex = 5;
+            this.btn_login.Text = "Log In";
+            this.btn_login.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Historic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(262, 465);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(261, 21);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Don\'t have an account? sign up here";
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 515);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.loginPasswordTextbox);
+            this.Controls.Add(this.loginUsernameTextbox);
+            this.Controls.Add(this.loginPasswordLabel);
+            this.Controls.Add(this.loginUsernameLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "loginForm";
+            this.Text = "loginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label loginUsernameLabel;
+        private System.Windows.Forms.Label loginPasswordLabel;
+        private System.Windows.Forms.TextBox loginUsernameTextbox;
+        private System.Windows.Forms.TextBox loginPasswordTextbox;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ signupForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace CourseCorrect
+{
+    partial class signupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_signup = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(146, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(467, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sign up CourseCorrect";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI Historic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(329, 150);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(295, 35);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI Historic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(329, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(295, 35);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Historic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(146, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Historic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(146, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 30);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Historic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(255, 388);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(205, 21);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Have an account? login here";
+            // 
+            // btn_signup
+            // 
+            this.btn_signup.Font = new System.Drawing.Font("Segoe UI Historic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_signup.Location = new System.Drawing.Point(294, 309);
+            this.btn_signup.Name = "btn_signup";
+            this.btn_signup.Size = new System.Drawing.Size(134, 44);
+            this.btn_signup.TabIndex = 5;
+            this.btn_signup.Text = "Sign up";
+            this.btn_signup.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe UI Historic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox1.Location = new System.Drawing.Point(434, 259);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 25);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Admin";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Historic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(285, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(152, 30);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Account Type";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Segoe UI Historic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox2.Location = new System.Drawing.Point(219, 259);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(83, 25);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Student";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // signupForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_signup);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "signupForm";
+            this.Text = "signupForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_signup;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
